8df5efa603dcac3dfbab97e7563a73bab7ba223f
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-04-18 11:15:53 +0000

    initial

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,7 +3,7 @@ import { Button } from 'primereact/button';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-
+import logo from "../images/logo.svg"
 import React from 'react';
 import { Menubar } from 'primereact/menubar';
 import { InputText } from 'primereact/inputtext';
@@ -13,140 +13,58 @@ export default function Navbar() {
 
     const items = [
         {
-            label: 'File',
-            icon: 'pi pi-fw pi-file',
-            items: [
-                {
-                    label: 'New',
-                    icon: 'pi pi-fw pi-plus',
-                    items: [
-                        {
-                            label: 'Bookmark',
-                            icon: 'pi pi-fw pi-bookmark'
-                        },
-                        {
-                            label: 'Video',
-                            icon: 'pi pi-fw pi-video'
-                        },
+            label: 'About me',
+            icon:'pi pi-fw pi-user'
 
-                    ]
-                },
-                {
-                    label: 'Delete',
-                    icon: 'pi pi-fw pi-trash'
-                },
-                {
-                    separator: true
-                },
-                {
-                    label: 'Export',
-                    icon: 'pi pi-fw pi-external-link'
-                }
-            ]
         },
         {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-                {
-                    label: 'Left',
-                    icon: 'pi pi-fw pi-align-left'
-                },
-                {
-                    label: 'Right',
-                    icon: 'pi pi-fw pi-align-right'
-                },
-                {
-                    label: 'Center',
-                    icon: 'pi pi-fw pi-align-center'
-                },
-                {
-                    label: 'Justify',
-                    icon: 'pi pi-fw pi-align-justify'
-                },
+            label: 'Education',
+            icon:'pi pi-fw pi-book'
 
-            ]
         },
         {
-            label: 'Users',
-            icon: 'pi pi-fw pi-user',
-            items: [
-                {
-                    label: 'New',
-                    icon: 'pi pi-fw pi-user-plus',
+            label: 'Experience',
+            icon:'pi pi-fw pi-user'
 
-                },
-                {
-                    label: 'Delete',
-                    icon: 'pi pi-fw pi-user-minus',
-
-                },
-                {
-                    label: 'Search',
-                    icon: 'pi pi-fw pi-users',
-                    items: [
-                        {
-                            label: 'Filter',
-                            icon: 'pi pi-fw pi-filter',
-                            items: [
-                                {
-                                    label: 'Print',
-                                    icon: 'pi pi-fw pi-print'
-                                }
-                            ]
-                        },
-                        {
-                            icon: 'pi pi-fw pi-bars',
-                            label: 'List'
-                        }
-                    ]
-                }
-            ]
         },
         {
-            label: 'Events',
-            icon: 'pi pi-fw pi-calendar',
-            items: [
-                {
-                    label: 'Edit',
-                    icon: 'pi pi-fw pi-pencil',
-                    items: [
-                        {
-                            label: 'Save',
-                            icon: 'pi pi-fw pi-calendar-plus'
-                        },
-                        {
-                            label: 'Delete',
-                            icon: 'pi pi-fw pi-calendar-minus'
-                        }
-                    ]
-                },
-                {
-                    label: 'Archieve',
-                    icon: 'pi pi-fw pi-calendar-times',
-                    items: [
-                        {
-                            label: 'Remove',
-                            icon: 'pi pi-fw pi-calendar-minus'
-                        }
-                    ]
-                }
-            ]
+            label: 'Projects',
+
         },
         {
-            label: 'Quit',
-            icon: 'pi pi-fw pi-power-off'
-        }
-        
+            label: 'Resume',
+
+} ,
+        {
+            label: 'Contact',
+            icon:'pi pi-fw pi-mobile'
+
+}
+
     ];
+    const style = {
+        backgroundColor: 'rgba(252,248,248,0.88)',
+        color: '#230202',
+        borderRadius:"20px",
+        justifyContent: 'left'
+    };
 
-    const start = <img alt="logo" src="showcase/images/logo.png" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholders.png'} height="100" className="mr-2"></img>;
+  //  const start = <img alt="logo" src="showcase/images/logo.png" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholders.png'} height="100" className="mr-2"></img>;
    // const end = <InputText placeholder="Search" type="text" />;
-
+    const end = (
+        <img
+            alt="logo"
+            src={logo}
+            height="20"
+            width="50"
+            //className="p-mr-2"
+            className="mr-2"
+        />
+    );
     return (
         <div>
             <div className="card">
-                <Menubar  start={start} model={items} />
+                <Menubar  end={end} style={style}  model={items}  />
 
             </div>
         </div>
