908e5f04a215d9f43f09e5d63ba297dc4bf2dfad
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-04-18 21:39:07 +0000

    educations

--- src/component/Education.js
+++ src/component/Education.js
@@ -1,14 +1,9 @@
 
-
-
-import 'swiper/css';
-
-
-
-import profile from "../images/personnalphoto.jpg"
-
 import { Card } from 'primereact/card';
-
+import logo from "../images/whitelogo.svg";
+import React from "react";
+import fst from "../images/fst.png"
+import emsi from "../images/emsi1.png"
 
 export default function Education() {
 
@@ -20,20 +15,46 @@ export default function Education() {
                 <section className="min-h-screen">
 
                     <div>
-                        <h3 className="text-4xl py-1  text-center dark:text-white font-burtons ">Education:</h3>
-                        <p className="text-md py-2  text-center leading-8 text-gray-800 dark:text-gray-200">
-                            Thanks to self education and these two establishments, I have a strong educational background in computer science.
-                        </p>
+                        <Card  >
+                            <h3 className="text-4xl py-1  text-center  font-burtons ">Education:</h3>
+                            <p className="text-center font-medium mt-5" >
+                                Thanks to self education and these two establishments, I have a strong educational background in computer science.
 
+                            </p>
+                        </Card>
+                    </div>
+                    <div className="mt-5">
+                        <Card  >
+                            <div  style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+
+                            <img alt="logo" src={fst} height="20" width="80" className="p-mr-2"/>
+                            </div>
+                            <p className="text-center font-semibold mt-5" >
+                                I obtained my bachelor s degree in Computer Science from the<span style={{color:"lightseagreen"}}> FST</span>  University,</p>
+                            <p className="text-center font-semibold mt-5" >
+                                where I gained a solid foundation in programming languages, and algorithms.
+                            </p>
+                        </Card>
+                    </div>
+                    <div className="mt-5">
+                        <Card  >
+                            <div  style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+
+                                <img alt="logo" src={emsi} height="20" width="80" className="p-mr-2"/>
+                            </div>
+                            <p className="text-center font-semibold mt-5" >
+                                I am closer to complete my engineering education at EMSI School,
+                            </p>
+                            <p className="text-center font-semibold mt-5" >
+                                where I deepened my understanding of computer architecture,
+                            </p>
+                            <p className="text-center font-semibold mt-5" >
+                                operating systems, and databases.
+                            </p>
+                        </Card>
                     </div>
                 </section>
-                <Card  >
-                    <p className="text-center font-medium " >
-                        I am highly interested in software development and learning new technologies.
-                        I have strong skills in programming and problem-solving,
-                        and I have participated in numerous software development projects as a part of a team.
-                    </p>
-                </Card>
+
             </main>
         </div>
     );
