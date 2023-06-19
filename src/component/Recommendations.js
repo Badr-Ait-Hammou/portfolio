1065a6eb2c93f6b123532a31a98e51c57c9e801c
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-06-19 10:43:49 +0100

    update

--- src/component/Recommendations.js
+++ src/component/Recommendations.js
@@ -1,7 +1,11 @@
 import React from "react"
 import "../styles/workingonit.css"
+import {Card} from "primereact/card";
+import { ProgressSpinner } from 'primereact/progressspinner';
+
 export default function Recommendations(){
     return(
+        <Card>
         <div className="card "  >
             <div>
                 <div className="starsec"></div>
@@ -15,15 +19,17 @@ export default function Recommendations(){
             <section className="error">
                 <div className="error__content">
                     <div className="error__message message">
+                        <ProgressSpinner style={{width: '50px', height: '50px',marginBottom:'5px'}} strokeWidth="3" fill="var(--surface-ground)" animationDuration=".9s" />
+
                         <h1 className="message__title" >WORK IN PROGRESS</h1>
-                        <p className="message__text"style={{fontFamily:"Peralta"}}>I am currently working on improving this part.</p>
+                        <p className="message__text" style={{fontFamily:"Peralta"}}>I am currently working on this part.</p>
                     </div>
 
 
                 </div>
             </section>
         </div>
-
+        </Card>
 
 
     );
